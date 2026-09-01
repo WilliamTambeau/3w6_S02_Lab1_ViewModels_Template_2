3e52e17158f02f3752cb386b1477271b00311887
William Tambeau <2572457@cegepmontpetit.ca>
2026-09-01 09:35:13 -0400

    FCT ZombieType Create ajout de message de confirmation
    Ajout d’un message de confirmation lors de la création d’un type de zombie.

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -3,42 +3,43 @@ using ZombieParty.Models;
 
 namespace ZombieParty.Controllers
 {
-    public class ZombieTypeController : Controller
-    {
-
-        private BaseDonnees _baseDonnees { get; set; }
-
-        public ZombieTypeController(BaseDonnees baseDonnees)
+	public class ZombieTypeController : Controller
 	{
-            _baseDonnees = baseDonnees;
+
+		private BaseDonnees _baseDonnees { get; set; }
+
+		public ZombieTypeController(BaseDonnees baseDonnees)
+	{
+			_baseDonnees = baseDonnees;
 	}
 
-        public IActionResult Index()
-        {
-            this.ViewBag.MaListe = _baseDonnees.ZombieTypes.ToList();
-            return View();
-        }
+		public IActionResult Index()
+		{
+			this.ViewBag.MaListe = _baseDonnees.ZombieTypes.ToList();
+			return View();
+		}
 
-        //GET CREATE
-        public IActionResult Create()
-        {
-            return View();
-        }
+		//GET CREATE
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        //POST
-        [HttpPost]
-        public IActionResult Create(Models.ZombieType zombieType)
-        {
-            if (ModelState.IsValid)
-            {
-                // Ajouter à la BD
-                _baseDonnees.ZombieTypes.Add(zombieType);
-		return this.RedirectToAction("Index");
+		//POST
+		[HttpPost]
+		public IActionResult Create(Models.ZombieType zombieType)
+		{
+			if (ModelState.IsValid)
+			{
+				// Ajouter à la BD
+				_baseDonnees.ZombieTypes.Add(zombieType);
+				TempData["Success"] = $"{zombieType.TypeName} zombie type added";
+				return this.RedirectToAction("Index");
 
-            }
+			}
 
-            return this.View(zombieType);
-        }
+			return this.View(zombieType);
+		}
 
-    }
+	}
 }
