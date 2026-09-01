842879e75d85de2b0e190657b5cd655a2e4fc48a
William Tambeau <2572457@cegepmontpetit.ca>
2026-09-01 09:17:59 -0400

    FCT Zombie Index et Create et Validation avec messages d’erreurs
    Maintenant possible de voir la liste de zombies et d’en ajouter avec gestion d’erreurs

--- ZombieParty/ZombieParty/Models/Zombie.cs
+++ ZombieParty/ZombieParty/Models/Zombie.cs
@@ -1,11 +1,11 @@
 ﻿namespace ZombieParty.Models
 {
-    public class Zombie
-    {
-        public string Name { get; set; }
-        public int Point { get; set; }
+	public class Zombie
+	{
+		public string Name { get; set; }
+		public int Point { get; set; }
 
 	public int ZombieTypeId { get; set; }
-	public ZombieType ZombieType { get; set; }
-    }
+	public ZombieType? ZombieType { get; set; }
+	}
 }
