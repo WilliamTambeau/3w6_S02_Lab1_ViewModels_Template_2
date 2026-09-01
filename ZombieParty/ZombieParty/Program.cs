eac644788e19ecd638992630ceb98dfee92a95e6
William Tambeau <2572457@cegepmontpetit.ca>
2026-09-01 08:28:53 -0400

    FCT Base de donnees en liste et liasons
    Ajout de la classe contenant la base de données en listes et ajout de la liaison entre Zombie et ZombieType. Injection du Singleton

--- ZombieParty/ZombieParty/Program.cs
+++ ZombieParty/ZombieParty/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// injection des depandances
+builder.Services.AddSingleton<BaseDonnees>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
